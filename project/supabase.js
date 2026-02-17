7e16989f0556ed4da6b7ae28cc4eb409d327c460
osintbogmonk <lorenzorenaissance@gmail.com>
2026-02-17 20:14:13 +0300

    Create supabase.js

---
+++ project/supabase.js
@@ -0,0 +1,7 @@
+// supabase.js
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://euzmecqnozwwrjiaqggt.supabase.co",
+  "sb_publishable_wQpw5zl350Zoj4AtkK5waA_s-I50Wbl"
+);
